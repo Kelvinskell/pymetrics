4d7ac8e7f83240d6adf306c345b2abf69ca71f69
Kelvinskell <Kelvinskelll@gmail.com>
2022-06-01 20:42:11 +0100

    feat: Collect general system information

---
+++ features/sys_info.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import psutil
+import socket
+import platform
+import distro
+import psutil
+from datetime import datetime
+from datetime import date
+
+class SysFetch():
+    def __init__(self, values):
+        self.boot_time = datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+        self.cpu_cores = psutil.cpu_count()
+        self.distro = distro.id()
+        self.nodename = socket.gethostname()
+        self.os_type = platform.system()
+        self.user = os.getlogin()
+        self.version = platform.release()
+        self.date = date.today()
+        self.time = datetime.now().strftime(%H:%M:%S)
