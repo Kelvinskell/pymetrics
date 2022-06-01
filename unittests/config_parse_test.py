2b08482221632563a8489ed59ae69ee194aa41e8
Kelvinskell <kelvinskelll@gmail.com>
2022-06-01 12:23:41 +0100

    test: Add unit test for config_parse

---
+++ unittests/config_parse_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+# Import unittest library
+import unittest
+
+# Import module for testing
+sys.path.append('../features')
+import config_parse
+
+# Instantiate YamlParser class
+parse_class = config_parse.YamlParser('../default.conf.yml')
+
+class TestYamlParser(unittest.TestCase):
+    def test_basic(self):
+        self.assertTrue(parse_class.yaml_to_python())
+
+unittest.main()
