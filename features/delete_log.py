3e42027927ba5fe7e4b4e996d7679666f586de8d
Kelvinskell <kelvinskelll@gmail.com>
2022-06-03 14:22:43 +0100

    feat: Add logs deletion feature

---
+++ features/delete_log.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+import os
+from datetime import timedelta
+
+class DeleteLog():
+    def __init__(self, values):
+        self.values = values
+
+    def formatDate(self):
+        nested_list = []
+        file_dates = []
+        pattern = r"([_a-zA-Z]+)-([\d-]+)(\.txt|\.csv|\.json)$"
+        if os.path.isdir('logs'):
+            for parentdir, subdir, namelists in os.walk("logs"):
+                nested_list.append(namelists)
+            # Extract each filename from list of filenames
+            flattened_list = [file for sublist in nested_list for file in sublist]
+
+            for filename in flattened_list:
+                regex = re.search(pattern, filename)
+                if regex:
+                    file_dates.append(regex.group(2))
+            self.filedates = file_dates
+            return self.filedates
+
