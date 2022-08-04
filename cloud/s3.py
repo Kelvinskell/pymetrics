b5bac42897bda5bac7705bf80da8c2757dcf93ec
Kelvinskell <kelvinskelll@gmail.com>
2022-08-04 10:43:00 +0100

    feat: Upload logs to s3 bucket

--- cloud/s3.py
+++ cloud/s3.py
@@ -7,7 +7,21 @@ def uploadS3(*args):
     region = args[0]
     bucket_name = args[1]
 
+    # Create AWS session
     session = boto3.Session(
             region_name=region
             )
+
+    # Connect session to S3
     s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    for subdir, dirs, files in os.walk('logs'):
+        for file in files:
+            absolute_path = os.path.join(subdir, file)
+            print(absolute_path)
+            with open(absolute_path, 'rb') as data:
+                bucket.put_object(Key=absolute_path, Body=data)
+         
+
+
+#uploadS3('us-east-1', 'pymetrics')
